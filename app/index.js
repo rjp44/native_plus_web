6f173ee6880d3f9b786e69703ba7b4d04e4e47d4
rjp44 <rob@ipcortex.co.uk>
2018-02-21 18:04:00 +0000

    tidy up builds

--- app/index.js
+++ app/index.js
@@ -15,19 +15,11 @@ AppRegistry,
 
 import { StackNavigator } from 'react-navigation';
 
-
 import { styles } from './config/styles.js';
 
 import { HomeScreen } from './screens/HomeScreen.js';
 import { ForwardScreen } from './screens/ForwardScreen.js';
 import { NightModeScreen } from './screens/NightModeScreen.js';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 const NativePlusWebApp = StackNavigator({
   Home: { screen: HomeScreen },
@@ -42,14 +34,5 @@ export default class App extends Component {
     );
   }
 }
-/*
-export default class App extends Component {
-  render() {
-    return (<View style={styles.container}>
-      <ForwardState title="Forwards"/>
-      <PhoneNumber title="Number"/>
-    </View>);
-  }
-}
-*/
+
 AppRegistry.registerComponent('NativePlusWebApp', () => NativePlusWebApp);
